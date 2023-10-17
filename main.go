14709492e29b2d66621f72f9965cbfcada3388a5
Sunimal Herath <sunimaldv@gmail.com>
2023-10-17 23:36:25 +1100

    Base64 authentication example

--- main.go
+++ main.go
@@ -1,68 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
+	"encoding/base64"
+	"fmt"
 )
 
-type person struct {
-	Name string
-}
-
 func main() {
-
-	// p2 := person{
-	// 	Name: "Lada",
-	// }
-
-	// xp := []person{p1, p2}
-
-	// bs, err := json.Marshal(xp)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-
-	// fmt.Println("JSON:", string(bs))
-
-	// xp2 := []person{}
-
-	// err = json.Unmarshal(bs, &xp2)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-
-	// fmt.Println("Go data structure:", xp2)
-
-	http.HandleFunc("/encode", foo)
-	http.HandleFunc("/decode", bar)
-	http.ListenAndServe(":8080", nil)
-}
-
-func foo(w http.ResponseWriter, r *http.Request) {
-	p1 := person{
-		Name: "Jeny",
-	}
-
-	p2 := person{
-		Name: "Lada",
-	}
-
-	xp := []person{p1, p2}
-
-	err := json.NewEncoder(w).Encode(xp)
-	if err != nil {
-		log.Println("Bad data to encode", err)
-	}
-}
-
-func bar(w http.ResponseWriter, r *http.Request) {
-	var xp []person
-
-	err := json.NewDecoder(r.Body).Decode(&xp)
-	if err != nil {
-		log.Println("Bad data to decode", err)
-	}
-
-	log.Println("Person", xp)
+	// Base64 basic authentication
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+	// the output of the above (dXNlcjpwYXNz) is the same as what you get from the below curl command.
+	// curl -u user:pass -v google.com will output => "Authorization: Basic dXNlcjpwYXNz"
+	// -u = basic authentication
+	// -v = verbose mode
+	// send the user:pass combination to google.com
 }
