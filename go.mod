37c323d7ccf2d719f7a36f1971313901151283a8
Sunimal Herath <sunimaldv@gmail.com>
2023-10-18 20:51:37 +1100

    adding hashing example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sunimalherath/go-inter-arch
 
 go 1.21.0
+
+require golang.org/x/crypto v0.14.0
--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	// Base64 basic authentication
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
-	// the output of the above (dXNlcjpwYXNz) is the same as what you get from the below curl command.
-	// curl -u user:pass -v google.com will output => "Authorization: Basic dXNlcjpwYXNz"
-	// -u = basic authentication
-	// -v = verbose mode
-	// send the user:pass combination to google.com
+	pass := "pass123456"
+
+	hashedPass, err := hashPassword(pass)
+	if err != nil {
+		panic(err)
+	}
+
+	err = comparePassword(pass, hashedPass)
+	if err != nil {
+		log.Fatalln("Not logged in")
+	}
+
+	log.Println("Logged in!")
+}
+
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("error while generating hash from password, %w", err)
+	}
+
+	return bs, nil
+}
+
+func comparePassword(password string, hashPassword []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashPassword, []byte(password))
+	if err != nil {
+		return fmt.Errorf("invalid password, %w", err)
+	}
+
+	return nil
 }
